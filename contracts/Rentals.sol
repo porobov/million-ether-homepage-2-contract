1ea8c1d2307b60a361415a64fde7bb03bebdef7b
porobov <porobov.p@gmail.com>
2018-06-29 20:44:40 +0400

    Rentals first sane architecture

--- contracts/Rentals.sol
+++ contracts/Rentals.sol
@@ -14,6 +14,7 @@ contract Rentals is MehModule {
     struct RentDeal {          
         address renter;  // block renter
         uint rentedFrom;  // time when rent started
+        uint numberOfPeriods;  //periods available
     }
     mapping(uint => RentDeal) public blockIdToRentDeal;
 
@@ -27,15 +28,13 @@ contract Rentals is MehModule {
         external
         onlyMeh
     {   
-        // collect rent
-        // cancel current rent
+        cutCurrentRentDeal(_blockId);  // claims earned for previos as well
         blockIdToRentPrice[_blockId] = _rentPricePerPeriodWei;
     }
 
     // MEH
     // rent out an area of blocks at coordinates [fromX, fromY, toX, toY]
     // hourlyRentForEachBlockInWei = 0 - not for rent
-    // function rentOutArea(uint8 fromX, uint8 fromY, uint8 toX, uint8 toY, uint rentForEachBlockCentsPerSecond, uint maxPeriodSeconds)
     function rentOutArea(uint8 fromX, uint8 fromY, uint8 toX, uint8 toY, uint _rentPricePerPeriodWei)
         external
         whenNotPaused
@@ -52,52 +51,108 @@ contract Rentals is MehModule {
         // LogRent(numRentStatuses, fromX, fromY, toX, toY, hourlyRentForEachBlockInWei, 0, address(0x0));
     }
 
-    function rentBlock (address renter, uint16 _blockId)
-        internal
-    {
-        createRentDeal(renter, _blockId, now);
-        firstRentPayMent(_blockId);  // TODO try to charge first and set deal second
+    function isForRent(uint16 _blockId) {
+        return (blockIdToRentPrice[_blockId] > 0);
     }
 
-    function createRentDeal(address _renter, uint16 _blockId, uint _rentedFrom) {
+    function isRented(uint16 _blockId) {
+        RentDeal deal = blockIdToRentDeal[_blockId];
+        return (deal.rentedFrom + deal.numberOfPeriods * rentPeriod > now);
+    }
+
+    function createRentDeal(address _renter, uint16 _blockId, uint _rentedFrom, uint _numberOfPeriods) {
         blockIdToRentDeal[_blockId].renter = _renter;
         blockIdToRentDeal[_blockId].rentedFrom = _rentedFrom;
+        blockIdToRentDeal[_blockId].numberOfPeriods = _numberOfPeriods;
     }
 
-    function firstRentPayMent(uint16 _blockId) {
-        chargeRent(_blockId, 1);
+    function removeRentDeal(uint16 _blockId) {
+        delete blockIdToRentDeal[_blockId];
     }
 
-    function chargeRent(uint16 _blockId, uint numberOfPeriods) {
+    function escrowRentPayment(uint16 _blockId, uint _totalRent) {
         uint rentPrice = numberOfPeriods * blockIdToRentPrice[_blockId];
         address renter = blockIdToRentDeal[_blockId].renter;
         address landlord = meh.ownerOf(_blockId)
         deductFrom(renter, rentPrice);
-        depositTo(landlord, rentPrice);
+        depositTo(rentOffice, rentPrice);
+
+    }
+    
+    function _numPeriodsElapsed (uint16 _blockId) {
+        return numPeriodsElapsed = (now - rentedFrom) / rentPeriod;  //TODO will auto-truncate
     }
 
-    function isRentedOut(uint16 _blockId) {
+    // does not affect renter if they have enough funds
+    // moves rented from, pays rent profit
+    function refreshRentDeal(uint16 _blockId) {
         
+        uint numPeriodsElapsed = _numPeriodsElapsed(_blockId);
+        require(numPeriodsElapsed > 0);
+        
+        // pay landlord
+        uint landlordPayment = numPeriodsElapsed * rentPrice;
+        deductFrom(rentOffice, landlordPayment);
+        address landlord = ownerOf(_blockId);
+        depositTo(landlord, landlordPayment);
+
+        // renew rent
+        uint numPeriodsLeft = numberOfPeriods - numPeriodsElapsed;
+        if (numPeriodsLeft >= 1) {
+            newRentedFrom = rentedFrom + numPeriodsElapsed * rentPeriod;
+            cutRentLeft(_blockId, newRentedFrom, numPeriodsLeft);
+        } else {
+            removeRentDeal(_blockId);
+        }
+        return numPeriodsLeft;
     }
 
-    function rentPriceForOnePeriod(uint8 fromX, uint8 fromY, uint8 toX, uint8 toY) {
-        require(isRentedOut(fromX, fromY, toX, toY));
-        require(hasNoRenter(fromX, fromY, toX, toY));
-        return rentPricePerPeriod;
+    // 
+    function cutCurrentRentDeal(uint16 _blockId) {
+        uint numPeriodsLeft = refreshRent(uint16 _blockId);
+        if (numPeriodsLeft == 1) {
+            //do nothing
+        }
+        if (numPeriodsLeft > 1) {
+            cutRentRight(_blockId, 1);
+            deductFrom(rentOffice, numPeriodsLeft * rentPrice);
+            depositTo(renter, numPeriodsLeft * rentPrice);
+        }
     }
 
-    function rentArea(uint8 fromX, uint8 fromY, uint8 toX, uint8 toY) // , uint rentPeriodHours) 
+    function rentPriceForOnePeriod(uint16 _blockId) {
+        require(isForRent(_blockId));
+        require(!(isRented(_blockId)));
+        return blockIdToRentPrice[_blockId];
+    }
+
+    // TODO prolong rent
+    // TODO pay previous owner
+    function rentBlock (address _renter, uint16 _blockId, uint _numberOfPeriods)
+        internal
+    {   
+        require(isForRent(_blockId));
+        require(!(isRented(_blockId)));  // TODO will throw but the rent will not refresh
+        refreshRentDeal(_blockId);  // TODO how to refresh if throws
+        escrowRentPayment(_blockId, renter, _numberOfPeriods);
+        createRentDeal(_renter, _blockId, now, _numberOfPeriods);
+    }
+
+    // what if tries to rent own area
+    // what if tries to rent mixed (rented by themselves + not rented yet)
+    function rentArea(uint8 fromX, uint8 fromY, uint8 toX, uint8 toY, uint _numberOfPeriods) // , uint rentPeriodHours) 
         external
         payable
     {   
         require(isLegalCoordinates(fromX, fromY, toX, toY));
-        require(msg.value >= rentPriceForOnePeriod(fromX, fromY, toX, toY));
+        require(msg.value >= rentPriceTotal(fromX, fromY, toX, toY, _numberOfPeriods));
+
         depositTo(msg.sender, msg.value);
 
         for (uint8 ix=fromX; ix<=toX; ix++) {
             for (uint8 iy=fromY; iy<=toY; iy++) {
                 uint16 _blockId = blockID(ix, iy);
-                rentBlock(msg.sender, _blockId);
+                rentBlock(msg.sender, _blockId, _numberOfPeriods);
             }
         }
         // numRentStatuses++;
